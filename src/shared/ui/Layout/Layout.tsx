e70ca9d8cdda8b4f98310cca23a07b794caeaa77
Sergey Bruska <sergeybruska@gmail.com>
2023-08-12 13:30:17 +0700

    fix: btn link styles

--- src/shared/ui/Layout/Layout.tsx
+++ src/shared/ui/Layout/Layout.tsx
@@ -30,7 +30,10 @@ export const Layout: FC<LayoutProps> = (props) => {
       <Head>
         <title>{title}</title>
         <meta name='description' content={description} />
-        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta
+          name='viewport'
+          content='width=device-width, initial-scale=1, maximum-scale=1'
+        />
         <link rel='icon' href='/favicon.ico' />
       </Head>
       {headerSlot}
